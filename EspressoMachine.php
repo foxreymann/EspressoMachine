cd95bf5a683e4c6f127c491282a3c7e99ec05198
tomaszjureczko <tomasz.jureczko@gmail.com>
2013-01-08 09:03:16 +0000

    skeleton of the app

---
+++ EspressoMachine.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'EspressoMachine.interface.php';
+
+abstract class EspressoMachine implements EspressoMachineInterface
+{
+
+
+
+
+}
---
+++ EspressoMachineTest.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'EspressoMachine.php';
+
+class EspressoMachineTest extends PHPUnit_Framework_TestCase
+{
+    public function test1() {
+        $this->assertEquals(1,1);
+    }
+}
